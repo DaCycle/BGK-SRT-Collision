50fcb76ec1029e41c5a03bca1ec47b2ae0b1c52b
Michael <145715300+DaCycle@users.noreply.github.com>
2025-07-14 16:40:36 -0700

    Finished benchmarking

--- main.cu
+++ main.cu
@@ -3,11 +3,13 @@
 int main()
 {
 	// Set different grid sizes
-	const int G[6] = { 12, 30, 90, 300, 600, 900 };
-	moment_ms_vec = new float[6];
+	const int G[7] = { 12, 30, 90, 300, 600, 900, 6000 };
+	int runs = 100;
 	
-	for (int i = 0; i < 6; ++i)
+	for (int i = 0; i < 7; ++i)
 	{
+		float moment_ms_vec = 0;
+
 		// Set size of the grid
 		const int g_x = G[i];
 		const int g_y = g_x;
@@ -18,6 +20,7 @@ int main()
 		double* h_U = new double[g_x * g_y * 2];
 
 		// Initialize the grid
+		// f(k, j, i) = h_f(k + (j * 9) + (i * 9 * g_x)) <-- Remember starts from 0
 		double f1[9] = { 1.63, 0.61, 0.41, 0.27, 0.41, 0.15, 0.07, 0.07, 0.16 };
 		double f2[9] = { 1.67, 0.42, 0.42, 0.42, 0.42, 0.10, 0.11, 0.10, 0.11 };
 		for (int i = 0; i < g_y; ++i) {
@@ -33,8 +36,6 @@ int main()
 			}
 
 		}
-		// Test grid initialization
-		cout << "f: " << h_f[4 + (0 * 9) + (1 * 9 * g_x)] << "\n"; // Remember starts from 0
 
 		// Allocate GPU memory
 		double* d_f;
@@ -47,7 +48,7 @@ int main()
 		// Copy grid from host to device
 		cudaMemcpy(d_f, h_f, g_x * g_y * 9 * sizeof(double), cudaMemcpyHostToDevice);
 
-		for (int j = 0; j < 10; ++j) {
+		for (int j = 0; j < runs; ++j) {
 
 			// Start kernel timing
 			cudaEvent_t start, stop;
@@ -67,12 +68,11 @@ int main()
 				// Calculate elapsed time
 				float moment_ms;
 				cudaEventElapsedTime(&moment_ms, start, stop);
-				moment_ms_vec[i] += moment_ms;
+				moment_ms_vec += moment_ms;
 			}
 
 			// Wait for GPU to finish before accessing on host
 			cudaDeviceSynchronize();
-
 		}
 
 		// Copy results back to host
@@ -83,6 +83,9 @@ int main()
 		//cout << "Rho: " << h_Rho[0] << ", " << h_Rho[1] << ", " << h_Rho[2] << ", " << h_Rho[3] << "\n";
 		//cout << "U: (" << h_U[0] << ", " << h_U[0 + g_x * g_y] << "), (" << h_U[1] << ", " << h_U[1 + g_x * g_y] << ")\n";
 		//cout << "Time taken for moment kernel execution: " << moment_ms << " ms\n";
+
+		// Output average time taken for each grid size
+		cout << "Average time taken for grid size " << g_x << ": " << moment_ms_vec / (runs - 5.0f) << " ms\n";
 
 		// Free allocated memory
 		delete[] h_f;
